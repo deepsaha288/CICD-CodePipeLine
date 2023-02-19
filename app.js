46ad2baef93ea16ba6842b1dffd6dee1bee1bd51
deepsaha288 <deepsaha288@yahoo.com>
2023-02-19 15:06:09 +0530

    new updated added into github

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("this is new build code!"));
+app.get("/", (req, res) => res.send("its work good now !"));
 
 app.get("/health", (req, res) => {
   res.status(200);
